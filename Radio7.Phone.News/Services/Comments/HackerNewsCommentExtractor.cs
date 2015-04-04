b24e1acc93b0f887a8b03e555e493b03e61731b5
benmcevoy <ben.mcevoy@gmail.com>
2015-04-04 16:31:20 +1100

    tweak some stuff

--- Radio7.Phone.News/Services/Comments/HackerNewsCommentExtractor.cs
+++ Radio7.Phone.News/Services/Comments/HackerNewsCommentExtractor.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using HtmlAgilityPack;
 using Radio7.Phone.News.Models;
@@ -44,17 +43,13 @@ namespace Radio7.Phone.News.Services.Comments
             if (commentTitle != null)
             {
                 var title = commentTitle.First().InnerText.Trim();
-                var end = title.IndexOf(" | ", StringComparison.Ordinal);
 
-                if (end > -1)
-                {
-                    result = "<h3>" + title.Substring(0, end).Trim() + "</h3>";
-                }
+                result = string.Format("<h3>{0}</h3>", title.Trim());
             }
 
             if (commentBody != null)
             {
-                result += "<div class='comment'>" + commentBody.First().InnerHtml.Trim() + "</div>";
+                result += string.Format("<div class='comment'>{0}</div>", commentBody.First().InnerHtml.Trim());
             }
 
             return result;
